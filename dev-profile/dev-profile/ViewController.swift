79eda649a9a75596c39176fe3f90a6a94efc244e
Aydogdy <aydogdy@Aydogdys-Mac.local>
2021-10-04 05:05:43 -0700

    initial project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Aydogdy on 10/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
