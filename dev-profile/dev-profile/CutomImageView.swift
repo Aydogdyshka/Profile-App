ed169c4ae3c85a91457a7a3cafd03befe3720cf2
Aydogdy <aydogdy@Aydogdys-Mac.local>
2021-10-07 04:31:10 -0700

    Builded an interface

---
+++ dev-profile/dev-profile/CutomImageView.swift
@@ -0,0 +1,16 @@
+//
+//  CutomImageView.swift
+//  dev-profile
+//
+//  Created by Aydogdy on 10/4/21.
+//
+
+import UIKit
+
+class CutomImageView: UIImageView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 10
+    }
+
+}
